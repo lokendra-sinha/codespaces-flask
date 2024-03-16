db3fcac8f48ca685f88fc9217ded9b16a8c0908d
Lokendra <124514819+lokendra-sinha@users.noreply.github.com>
2024-03-16 17:05:54 +0000

    final commit

--- app.py
+++ app.py
@@ -5,3 +5,27 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/name")
+
+def name():
+
+    return "Mithanshu"
+
+@app.route("/age")
+
+def age():
+
+    return "21"
+
+@app.route("/hello")
+
+def hello():
+
+    return "hello"
+
+@app.route("/goodbye")
+
+def goodbye():
+
+    return "goodbye"
